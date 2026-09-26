5aa1d464f8a9acf232319045ec369232794c3925
Naitik katyayan <naitikkatyayan@Naitiks-MacBook-Air.local>
2026-09-26 13:32:03 +0530

    Update calling app

--- app.py
+++ app.py
@@ -124,9 +124,11 @@ def handle_disconnect():
             break
 
 
+import os
+
 if __name__ == '__main__':
     socketio.run(
         app,
         host='0.0.0.0',
-        port=5000
+        port=int(os.environ.get('PORT', 5000))
     )
